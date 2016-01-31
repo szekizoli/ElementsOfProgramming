eaf0b297323c75dc9a1856f461e708fd40301a96
Zoltan Szekeres <szekizoli@gmail.com>
2016-01-31 19:43:53 +0100

    Move source to src\eop folder and add unit test

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,6 @@
 solution "ElementsOfProgramming-Exercises"
     configurations  { "Debug", "Release" }
-    startproject    "GraphLib-unittest"
+    startproject    "eop-unittest"
 
     location "build"
 
@@ -9,13 +9,51 @@ project "eop"
     language  "C++"
     targetdir "build/bin/%{cfg.buildcfg}"
 
-    files { "src/**.h", 
-            "src/**.cpp" }
+    files { "src/eop/**.h", 
+            "src/eop/**.cpp" }
 
     includedirs { "src" }
     filter "configurations:Debug"
     	defines {"DEBUG"}
     	flags {"Symbols"}
+    
+    filter "configurations:Release"
+        defines {"NDEBUG"}
+        optimize "On"
+
+project "GoogleTest"
+    kind      "StaticLib"
+    language  "C++"
+    targetdir "build/bin/%{cfg.buildcfg}"
+
+    files { "../3rd/gtest-1.7.0/src/gtest_main.cc", 
+            "../3rd/gtest-1.7.0/src/gtest-all.cc" }
+
+    libdirs { "../3rd/" }
+    includedirs {"../3rd/gtest-1.7.0", "../3rd/gtest-1.7.0/include"}
+
+    filter "configurations:Debug"
+        defines {"DEBUG"}
+        flags {"Symbols"}
+    
+    filter "configurations:Release"
+        defines {"NDEBUG"}
+        optimize "On"
+
+project "eop-unittest"
+    kind      "ConsoleApp"
+    language  "C++"
+    targetdir "build/bin/%{cfg.buildcfg}"
+
+    files { "src/unittest/**.h", 
+            "src/unittest/**.cpp" }
+
+    links {"eop", "GoogleTest"}
+    libdirs { "../3rd/" }
+    includedirs {"../3rd/gtest-1.7.0", "../3rd/gtest-1.7.0/include", "src/eop" }
+    filter "configurations:Debug"
+        defines {"DEBUG"}
+        flags {"Symbols"}
     
     filter "configurations:Release"
         defines {"NDEBUG"}
